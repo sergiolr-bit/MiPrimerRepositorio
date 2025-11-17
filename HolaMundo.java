00001cc869c0215347564d8316cf04f16eafe8ab
Sergio <sergiolopezreyes5476@gmail.com>
2025-11-17 19:13:17 +0000

    Primer commit

---
+++ HolaMundo.java
@@ -0,0 +1,9 @@
+package holamundo;
+import javax.swing.JOptionPane;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.printf("¡Hola Mundo!");
+        JOptionPane.showMessageDialog(null, "¡Hola Mundo!");
+    }
+}
